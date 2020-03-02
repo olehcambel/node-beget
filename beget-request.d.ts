3a25c78b17d416c0c619ec31b9a35430b2a8de99
olehcambel <nickcambel5@gmail.com>
2020-03-02 21:38:35 +0200

    Updates

--- beget-request.d.ts
+++
@@ -1,9 +0,0 @@
-import { BegetOptions } from './options/beget-options';
-import * as Beget from './types/controller.interface';
-export declare class BegetRequest {
-    private request;
-    private readonly config;
-    constructor(config: BegetOptions);
-    private safeRequest;
-    api<T extends Beget.Controller, R>(section: T, method: Beget.Methods[T], data?: object): Promise<R>;
-}
--- beget.error.d.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorPayload } from './types/common.interface';
-export declare class BegetError extends Error {
-    payload: ErrorPayload | ErrorPayload[];
-    on?: object | undefined;
-    name: string;
-    fatal: boolean;
-    constructor(payload: ErrorPayload | ErrorPayload[], on?: object | undefined);
-}
